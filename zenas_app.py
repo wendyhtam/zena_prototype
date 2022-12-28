c6b365419b404da1cfc74e7621e20645c45f74b1
Wendy T <105395847+wendyhtam@users.noreply.github.com>
2022-12-28 16:38:40 -0500

    Update zenas_app.py
    imported packages
    retrieve color and styles for catalog

--- zenas_app.py
+++ zenas_app.py
@@ -1,4 +1,16 @@
 import streamlit
 import pandas
+import snowflake.connector
+
+# connect to snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+# Retrieve catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+df = pandas.dataframe(my_catalog)
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
+streamlit.write(df)
+#streamlit.selectbox('Pick a sweatsuit color or style:', 
